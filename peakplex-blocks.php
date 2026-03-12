86c72f2aa41dcc54d25f28ff92a1d8e196c943a7
Ryan Legge <ryanlegge@Ryans-MacBook-Pro.local>
2026-03-12 22:44:58 +0000

    Update version

--- peakplex-blocks.php
+++ peakplex-blocks.php
@@ -2,10 +2,22 @@
 /**
  * Plugin Name: Peakplex Blocks
  * Description: Reusable blocks used across my websites.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Peakplex Internet
  */
 
 if (!defined('ABSPATH')) {
     exit;
 }
+
+require __DIR__ . '/plugin-update-checker/plugin-update-checker.php';
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$updateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/Peakplex/peakplex-blocks/',
+    __FILE__,
+    'peakplex-blocks'
+);
+
+$updateChecker->setBranch('main');
