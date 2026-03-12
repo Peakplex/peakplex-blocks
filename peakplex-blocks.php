c4ba404a767a075dec7c747ebc6df04159626e43
Ryan Legge <ryanlegge@Ryans-MacBook-Pro.local>
2026-03-12 17:08:12 +0000

    Add plugin files with content

---
+++ peakplex-blocks.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Peakplex Blocks
+ * Description: Reusable blocks used across my websites.
+ * Version: 1.0.0
+ * Author: Peakplex Internet
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
